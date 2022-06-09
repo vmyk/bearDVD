a01225a5c6e1a286b3a1a02ca1bbc42c5a81b8a7
mklz <calimiga70@gmail.com>
2022-06-09 22:46:33 +0100

    update emotes

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
 const bear = [
-  "https://cdn.7tv.app/emote/603cbda573d7a5001441f9d5/4x", //flushE
+
+  "https://cdn.frankerfacez.com/emoticon/274406/4", //3Head
+  "https://cdn.frankerfacez.com/emoticon/654991/4", //Bedge
+  "https://cdn.frankerfacez.com/emoticon/249060/4", //forsenCD
+  "https://cdn.frankerfacez.com/emoticon/139407/4", //LULW
+  "https://cdn.frankerfacez.com/emoticon/355871/4", //PepeLa
+  "https://cdn.frankerfacez.com/emoticon/457124/4", //WICKED
+  "https://cdn.betterttv.net/emote/60203910381eb06cbaa67ee3/3x", //Lamonting
+  "https://cdn.betterttv.net/emote/618f18da54f3344f8805695c/3x", //PepeHands
+  "https://cdn.betterttv.net/emote/61c4da14002cdeedc2206ccb/3x", //KEKW
+  "https://cdn.betterttv.net/emote/6029d83f82b7c45eb1c9484a/3x", //gachiHop
+  "https://cdn.betterttv.net/emote/60bb1554f8b3f62601c38d78/3x", //TriKool
+  "https://cdn.betterttv.net/emote/60e9e9e58ed8b373e42208bf/3x", //Drake
+  "https://cdn.betterttv.net/emote/61ad09e5002cdeedc21e5a97/3x", //PianoTime
+  "https://cdn.betterttv.net/emote/61c24c6d002cdeedc2203ace/3x", //PogU
+  "https://cdn.betterttv.net/emote/600c004c4e3ab965ef757f9e/3x", //KEKL
+  "https://cdn.betterttv.net/emote/583089f4737a8e61abb0186b/3x", //OMEGALUL
+  "https://cdn.betterttv.net/emote/61ed974806fd6a9f5be1d7a5/3x", //WAYTOODANK
+  "https://cdn.betterttv.net/emote/5c27ad590a1e447dea70e0c8/3x", //Pepege
+  "https://cdn.betterttv.net/emote/5fedf9a460488f2e470f143a/3x", //PauseMan
+  "https://cdn.betterttv.net/emote/609431bc39b5010444d0cbdc/3x", //GIGACHAD
+  "https://cdn.betterttv.net/emote/6111162076ea4e2b9f763379/3x", //AMEN
+  "https://cdn.betterttv.net/emote/618c4f1b1f8ff7628e6d56ab/3x", //bubbsJAM
+  "https://cdn.betterttv.net/emote/61d4fa7206fd6a9f5bdf6433/3x", //bubbsHop
+  "https://cdn.betterttv.net/emote/61a5f6ef371b825d3f6dd1b2/3x", //bearPlush
+  "https://cdn.betterttv.net/emote/61deabc606fd6a9f5be05fcd/3x", //bubbsTalk
+  "https://cdn.betterttv.net/emote/61f422f406fd6a9f5be27eb6/3x", //DonoWall
+  "https://cdn.betterttv.net/emote/61f03cb606fd6a9f5be2201d/3x", //Madge
+  "https://cdn.betterttv.net/emote/61f4410c06fd6a9f5be28204/3x", //DinkDonk
+  "https://cdn.betterttv.net/emote/61f4bd4b06fd6a9f5be290ae/3x", //peepoArrive
+  "https://cdn.betterttv.net/emote/6212d52606fd6a9f5be52323/3x", //Pepepains
+  "https://cdn.betterttv.net/emote/61f6af6306fd6a9f5be2bd7d/3x", //PepoCheer
+  "https://cdn.betterttv.net/emote/61f6af3b06fd6a9f5be2bd73/3x", //Comfy
+  "https://cdn.betterttv.net/emote/61f4bda806fd6a9f5be290bf/3x", //pepeJAM
+  "https://cdn.7tv.app/emote/62477a2efb18d86dec72ad34/4x", //Barry63
+  "https://cdn.betterttv.net/emote/618f18da54f3344f8805695c/3x", //PepeHands
+  "https://cdn.betterttv.net/emote/618f18da54f3344f8805695c/3x", //PepeHands
 
 ];
 
